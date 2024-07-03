2fa259e6c1f8df7658afd511157396d491f2db58
Roby23sy <roby23sy@yahoo.com>
2024-07-03 16:52:37 +0300

    First design

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="navbar-container">
+      <TabComponent />
+    </div>
+  </template>
+  
+  <script>
+  import TabComponent from "@/components/subcomponents/TabComponent.vue"
+  export default {
+    name: 'NavBar',
+    components: {
+        TabComponent
+    }
+  }
+  </script>
+  
+  <style>
+  </style>
+  
---
+++ src/components/subcomponents/TabComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tab-container">
+      <div class="tab-name"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TabComponent',
+    components: {
+    }
+  }
+  </script>
+  
+  <style>
+  </style>
+  
